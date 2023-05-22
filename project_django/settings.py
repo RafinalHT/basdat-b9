3abe1a434e9ce084adaf3af5ecc5af7ffadaee0b
AndiAyuna14 <andiayuna14@gmail.com>
2023-05-22 22:32:56 +0700

    Add pertandingan

--- project_django/settings.py
+++ project_django/settings.py
@@ -39,18 +39,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-<<<<<<< HEAD
-    'example_app',
-    'hasil_pertandingan',
-    'pertandingan',
-=======
     'sponsor',
     'pelatih',
     'home',
     'dashboard',
     'authentication',
     'test_kualifikasi',
->>>>>>> 6f6df79d5976c52469c5204170b4851fc1e1fa39
+    'hasil_pertandingan',
+    'pertandingan',
 ]
 
 MIDDLEWARE = [
