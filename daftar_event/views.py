ff2717927db941d793f2df7482912d6756b42da3
rafinal.haryokusumo <rafinal.haryokusumo@ui.ac.id>
2023-05-25 16:05:59 +0700

    daftar event not done

--- daftar_event/views.py
+++ daftar_event/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+from ast import parse
+from django.shortcuts import render, redirect
+from django.http import HttpResponse, HttpResponseRedirect
+from utils import query
+from django.contrib import messages
+import datetime
 
 # Create your views here.
 def pilih_stadium(request):
-    return render(request, 'pilih_stadium.html')
+    context={}
+    query_stadium = query.query(f"""
+    SELECT
+        Nama,
+        Negara,
+        Kapasitas
+    FROM STADIUM
+    """)
+    print(query_stadium)
+    list_stadium = [col[0] for col in query_stadium]
+    print(list_stadium)
+    context['stadium'] = list_stadium
+    return render(request, 'pilih_stadium.html', context)
 
 def pilih_event(request):
     return render(request, 'pilih_event.html')
